338a52ab52f65b5511907dd2710396b457c1524d
syukoGit <syukodev@outlook.com>
2023-03-03 11:50:25 +0100

    init project

---
+++ src/main/java/fr/lataverne/customix/Customix.java
@@ -0,0 +1,20 @@
+package fr.lataverne.customix;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * This class is the main class of the Customix plugin.
+ */
+public class Customix extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/fr/lataverne/customix/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains all the classes used to manage the Customix application.
+ */
+
+package fr.lataverne.customix;
